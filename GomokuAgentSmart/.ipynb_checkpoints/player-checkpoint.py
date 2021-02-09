be37e4a6b402be9bc24d40e16d4934e4efb796f9
sam lawson <samuel.n.lawson@gmail.com>
2021-02-09 17:21:16 +0000

    the smart ai will act randomly unless there is a winning move TEST

--- GomokuAgentSmart/.ipynb_checkpoints/player-checkpoint.py
+++ GomokuAgentSmart/.ipynb_checkpoints/player-checkpoint.py
@@ -1,12 +1,32 @@
 import numpy as np
 
 from misc import legalMove
+from misc import rowTest
+from misc import diagTest
 from gomokuAgent import GomokuAgent
 
 class Player(GomokuAgent):
     def move(self, board):
+        win = getWinningMove(self,board)
+        if(win):
+            return win
         while True:
             moveLoc = tuple(np.random.randint(self.BOARD_SIZE, size=2))
             print(moveLoc)
             if legalMove(board, moveLoc):
                 return moveLoc
+    
+    #check if there is a move that will win the game and return it else return false
+    def getWinningMove(self,board):
+        for i in range(BOARD_SIZE):
+            for j in range(BOARD_SIZE):
+                check = (i,j)
+                tempBoard = board
+                if legalMove(tempBoard, check):
+                    tempBoard[check] = self.ID
+                    if(rowTest(self.ID, tempBoard, self.X_IN_A_LINE)):
+                        return check
+                    if(diagTest(self.ID, tempBoard, self.X_IN_A_LINE)):
+                        return check
+        return False
+  
